ac420fa204b194a2c429dca8f1ae17c26e7b8562
Andreas Bakseter <andreas_tkd@hotmail.com>
2023-01-24 13:02:56 +0100

    ✨ pwa

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -5,10 +5,13 @@ const Document = () => {
         <Html lang="nb">
             <Head>
                 <meta name="robots" content="follow, index" />
+                <meta name="description" content="5/3/1 Program" />
                 <meta property="og:type" content="website" />
                 <meta name="apple-mobile-web-app-capable" content="yes" />
                 <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+                <meta name="theme-color" content="#ffffff" />
 
+                <link rel="apple-touch-icon" sizes="180x180" href="/ios/512.png" />
                 <link rel="manifest" href="/site.webmanifest" />
             </Head>
             <body>
