e9e963c18e2673e47734d93fabef666212ce8d5a
Andreas Bakseter <andreas_tkd@hotmail.com>
2023-03-05 00:17:44 +0100

    ➕ analytics

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,17 +1,21 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { SessionProvider } from 'next-auth/react';
+import { Analytics } from '@vercel/analytics/react';
 import { BaseWeightsProvider } from '@hooks/use-base-weights';
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
     return (
-        <SessionProvider session={session}>
-            <BaseWeightsProvider>
-                <ChakraProvider>
-                    <Component {...pageProps} />
-                </ChakraProvider>
-            </BaseWeightsProvider>
-        </SessionProvider>
+        <>
+            <SessionProvider session={session}>
+                <BaseWeightsProvider>
+                    <ChakraProvider>
+                        <Component {...pageProps} />
+                    </ChakraProvider>
+                </BaseWeightsProvider>
+            </SessionProvider>
+            <Analytics />
+        </>
     );
 };
 
