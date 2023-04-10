1e1288066bd14f8ee540055af785f849106cd958
Andreas Bakseter <andreas_tkd@hotmail.com>
2023-04-10 04:27:03 +0200

    🔧 increase cycles

--- frontend/src/lib/utils/constants.ts
+++ frontend/src/lib/utils/constants.ts
@@ -1,6 +1,6 @@
 import { type Week, type Day } from '@api/workout';
 
-const cycles: Array<number> = [1, 2, 3];
+const cycles: Array<number> = [1, 2, 3, 4, 5];
 const weeks: Array<Week> = [1, 2, 3];
 const days: Array<Day> = [1, 2, 3, 4];
 
