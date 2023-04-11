90c001cd0346c71128096c6086079020081edf96
Andreas Bakseter <andreas_tkd@hotmail.com>
2023-04-11 12:56:51 +0200

    🔥 rm -rf / --no-preserve-root

--- frontend2/src/hooks.server.ts
+++
@@ -1,9 +0,0 @@
-import { SvelteKitAuth } from '@auth/sveltekit';
-import Google from '@auth/core/providers/google';
-import { GOOGLE_ID, GOOGLE_SECRET } from '$env/static/private';
-
-export const handle = SvelteKitAuth({
-    providers: [
-        Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET }),
-    ],
-});
--- frontend2/src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    export let data;
-</script>
-
-<h1>{data.dl}</h1>
-<h1>{data.bp}</h1>
-<h1>{data.sq}</h1>
-<h1>{data.op}</h1>
