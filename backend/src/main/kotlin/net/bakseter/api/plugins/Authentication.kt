75240f0b72889bfea08bc26efbc118e45403be85
Andreas Bakseter <andreas_tkd@hotmail.com>
2023-09-19 15:22:17 +0200

    🐛 Bump next-auth and fix jwt issue

--- backend/src/main/kotlin/net/bakseter/api/plugins/Authentication.kt
+++ backend/src/main/kotlin/net/bakseter/api/plugins/Authentication.kt
@@ -12,18 +12,19 @@ import java.net.URL
 import java.util.concurrent.TimeUnit
 
 fun Application.configureAuthentication(devSecret: String, devAudience: String, devIssuer: String) {
-    val issuer = "https://www.googleapis.com/oauth2/v3/certs"
-    val jwkProvider = JwkProviderBuilder(URL(issuer))
+    val jwkIssuer = "https://www.googleapis.com/oauth2/v3/certs"
+    val jwkProvider = JwkProviderBuilder(URL(jwkIssuer))
         .cached(10, 24, TimeUnit.HOURS)
         .rateLimited(10, 1, TimeUnit.MINUTES)
         .build()
 
     install(Authentication) {
+        val issuer = "https://accounts.google.com"
         jwt("auth-user") {
             realm = "User access."
             verifier(jwkProvider, issuer) {
                 acceptLeeway(10)
-                withIssuer("https://accounts.google.com")
+                withIssuer(issuer)
             }
             validate {
                 JWTPrincipal(it.payload)
