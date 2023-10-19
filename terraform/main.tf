a01590b32b7e73de650f6cf22c748e6c9c0d3157
Andreas Bakseter <andreas_tkd@hotmail.com>
2023-10-19 19:11:43 +0200

    bump

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "2.26.0"
+      version = "2.30.0"
     }
     vercel = {
       source  = "vercel/vercel"
-      version = "0.11.4"
+      version = "0.15.2"
     }
   }
 }
@@ -76,21 +76,23 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-data "digitalocean_image" "img" {
-  slug = "docker-20-04"
-}
-
 resource "digitalocean_ssh_key" "ssh" {
   name       = var.ssh_key_name
   public_key = file(var.ssh_key_path)
 }
 
+// This should be used for specifying digitalocean_droplet.drop.image
+// data "digitalocean_image" "img" {
+//  slug = "docker-20-04"
+// }
+
 resource "digitalocean_droplet" "drop" {
-  image    = data.digitalocean_image.img.id
+  image    = "122088422"
   name     = "backend-1"
   region   = "ams3"
   size     = "s-1vcpu-1gb"
   ssh_keys = [digitalocean_ssh_key.ssh.fingerprint]
+  backups  = true
 
   lifecycle {
     prevent_destroy = true
@@ -99,6 +101,7 @@ resource "digitalocean_droplet" "drop" {
 
 resource "digitalocean_project" "project" {
   name        = "531"
+  environment = "Production"
   description = "5/3/1 workout plan"
   purpose     = "Web backend"
   resources = [
