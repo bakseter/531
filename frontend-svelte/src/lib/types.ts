c75f10281d13eaeb2274a2b2f0f57e1f0b21d992
Andreas Bakseter <andreas_tkd@hotmail.com>
2023-11-16 19:58:57 +0100

    svelte wip

---
+++ frontend-svelte/src/lib/types.ts
@@ -0,0 +1,43 @@
+import { type decodeType, number, record, intersection } from 'typescript-json-decoder';
+
+const baseWeightsDecoder = record({
+	dl: number,
+	bp: number,
+	sq: number,
+	op: number
+});
+type BaseWeights = decodeType<typeof baseWeightsDecoder>;
+
+type CompExercise = keyof BaseWeights;
+
+const baseWeightsModifierDecoder = intersection(baseWeightsDecoder, record({ cycle: number }));
+type BaseWeightsModifier = decodeType<typeof baseWeightsModifierDecoder>;
+
+const weekDecoder = (value: unknown) => {
+	if (value === 1 || value === 2 || value === 3) return value;
+	throw new TypeError('Invalid week');
+};
+type Week = decodeType<typeof weekDecoder>;
+
+const dayDecoder = (value: unknown) => {
+	if (value === 1 || value === 2 || value === 3 || value === 4) return value;
+	throw new TypeError('Invalid day');
+};
+type Day = decodeType<typeof dayDecoder>;
+
+const workoutDecoder = record({
+	cycle: number,
+	week: weekDecoder,
+	day: dayDecoder,
+	reps: number
+});
+type Workout = decodeType<typeof workoutDecoder>;
+
+const profileDecoder = (value: unknown) => {
+	if (value === 1 || value === 2 || value === 3 || value === 4) return value;
+	throw new TypeError('Invalid profile');
+};
+type Profile = decodeType<typeof profileDecoder>;
+
+
+export { baseWeightsDecoder, baseWeightsModifierDecoder, type CompExercise, type BaseWeights, type BaseWeightsModifier, weekDecoder, dayDecoder, workoutDecoder, profileDecoder, type Week, type Day, type Workout, type Profile };
---
+++ frontend-svelte/src/routes/+layout.server.ts
@@ -0,0 +1,15 @@
+import { redirect } from '@sveltejs/kit';
+import BaseWeightsAPI from '$lib/server/base-weights';
+import type { LayoutServerLoad } from './$types';
+
+const load: LayoutServerLoad = async (event) => {
+	const session = await event.locals.getSession();
+
+	if (!session?.idToken) throw redirect(307, '/auth/signin');
+
+	const baseWeights = await BaseWeightsAPI.getBaseWeights({ profile: 1, idToken: session?.idToken });
+
+	if (baseWeights) throw redirect(307, `/week/1/cycle/1`);
+};
+
+export { load };
